13914c84341b78148db9ef41798d2dec598f9954
itrinia <94580529+itrinia@users.noreply.github.com>
2023-04-11 08:44:03 +0700

    test

--- 0706012110019-ileenetrinia-AFL3/ContentView.swift
+++ 0706012110019-ileenetrinia-AFL3/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+//        This is view
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
