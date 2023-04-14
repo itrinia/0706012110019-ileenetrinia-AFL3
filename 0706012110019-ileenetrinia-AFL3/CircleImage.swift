e3800a53b3b213fa0c8e2e80a80b5b43e3e44991
itrinia <94580529+itrinia@users.noreply.github.com>
2023-04-14 09:07:46 +0700

    Section 4 Create a Custom Image View

---
+++ 0706012110019-ileenetrinia-AFL3/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  0706012110019-ileenetrinia-AFL3
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+
+
