d59e5915a6ed30cda4bf637adf9f3a00b5eb0aba
itrinia <94580529+itrinia@users.noreply.github.com>
2023-04-18 08:37:36 +0700

    chapter 3 - Section 3 Add a Control to Toggle the State

--- 0706012110019-ileenetrinia-AFL3/Views/LandmarkList.swift
+++ 0706012110019-ileenetrinia-AFL3/Views/LandmarkList.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
+    @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
@@ -24,24 +24,30 @@ struct LandmarkList: View {
         //            LandmarkRow(landmark: landmarks[0])
         //            LandmarkRow(landmark: landmarks[1])
         NavigationView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
                 }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
             }
-            .navigationTitle("Landmarks")
         }
     }
-}
-
-struct LandmarkList_Previews: PreviewProvider {
-    static var previews: some View {
-        //        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-        //            LandmarkList()
-        //                .previewDevice(PreviewDevice(rawValue: deviceName))
-        //                .previewDisplayName(deviceName)
-        LandmarkList()
+    
+    struct LandmarkList_Previews: PreviewProvider {
+        static var previews: some View {
+            //        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            //            LandmarkList()
+            //                .previewDevice(PreviewDevice(rawValue: deviceName))
+            //                .previewDisplayName(deviceName)
+            LandmarkList()
+        }
     }
 }
