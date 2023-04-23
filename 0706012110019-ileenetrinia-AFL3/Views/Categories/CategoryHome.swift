b3106ad09ffdd8b81f868e5633fb9a90b656424e
itrinia <94580529+itrinia@users.noreply.github.com>
2023-04-23 20:50:40 +0700

    c3 sc1 Section 1 Add a Category View

---
+++ 0706012110019-ileenetrinia-AFL3/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  0706012110019-ileenetrinia-AFL3
+//
+//  Created by MacBook Pro on 23/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
