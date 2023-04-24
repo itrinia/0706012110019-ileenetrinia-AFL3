8efb1af97ceabfa40001c9344de258583609cf0e
itrinia <94580529+itrinia@users.noreply.github.com>
2023-04-24 20:58:15 +0700

    c3 sc2 Section 3 Define the Profile Editor

---
+++ 0706012110019-ileenetrinia-AFL3/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,57 @@
+//
+//  ProfileEditor.swift
+//  0706012110019-ileenetrinia-AFL3
+//
+//  Created by MacBook Pro on 24/04/23.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+            
+            //toggle yg on off
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable Notifications").bold()
+            }
+            
+            //memilih emoji seasonal photo - kok lucu plzz
+            // ini pke for each biar gausah describe satu2 jd lgsg for each datanya season itu
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Seasonal Photo").bold()
+                
+                Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases) { season in
+                        Text(season.rawValue).tag(season)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            //tanggal tp hrs ada var datarange kyk di atas
+            DatePicker(selection: $profile.goalDate, in: dateRange, displayedComponents: .date) {
+                Text("Goal Date").bold()
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
